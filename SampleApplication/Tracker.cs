9e680c7c6f64cdd6ffb41a276af07fa5dd9a5ffc
ANDRELUIZKF\andre <andreluizkf@gmail.com>
2020-11-13 18:52:57 -0300

    Adicionar arquivos de projeto.

---
+++ SampleApplication/Tracker.cs
@@ -0,0 +1,184 @@
+﻿using MouseKeyboardLibrary;
+using System;
+using System.Windows.Forms;
+
+namespace SampleApplication
+{
+    public partial class Tracker : Form
+    {
+
+        MouseHook mouseHook = new MouseHook();
+        KeyboardHook keyboardHook = new KeyboardHook();
+
+        public Tracker()
+        {
+            InitializeComponent();
+        }
+
+        private void TestForm_Load(object sender, EventArgs e)
+        {
+
+            mouseHook.MouseMove += new MouseEventHandler(mouseHook_MouseMove);
+            mouseHook.MouseDown += new MouseEventHandler(mouseHook_MouseDown);
+            mouseHook.MouseUp += new MouseEventHandler(mouseHook_MouseUp);
+            mouseHook.MouseWheel += new MouseEventHandler(mouseHook_MouseWheel);
+
+            keyboardHook.KeyDown += new KeyEventHandler(keyboardHook_KeyDown);
+            keyboardHook.KeyUp += new KeyEventHandler(keyboardHook_KeyUp);
+            keyboardHook.KeyPress += new KeyPressEventHandler(keyboardHook_KeyPress);
+
+            mouseHook.Start();
+            keyboardHook.Start();
+
+            SetXYLabel(MouseSimulator.X, MouseSimulator.Y);
+
+        }
+
+        void keyboardHook_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            AddKeyboardEvent(
+                "KeyPress",
+                "",
+                e.KeyChar.ToString(),
+                "",
+                "",
+                ""
+                );
+
+        }
+
+        void keyboardHook_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            AddKeyboardEvent(
+                "KeyUp",
+                e.KeyCode.ToString(),
+                "",
+                e.Shift.ToString(),
+                e.Alt.ToString(),
+                e.Control.ToString()
+                );
+
+        }
+
+        void keyboardHook_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            AddKeyboardEvent(
+                "KeyDown",
+                e.KeyCode.ToString(),
+                "",
+                e.Shift.ToString(),
+                e.Alt.ToString(),
+                e.Control.ToString()
+                );
+
+        }
+
+        void mouseHook_MouseWheel(object sender, MouseEventArgs e)
+        {
+
+            AddMouseEvent(
+                "MouseWheel",
+                "",
+                "",
+                "",
+                e.Delta.ToString()
+                );
+
+        }
+
+        void mouseHook_MouseUp(object sender, MouseEventArgs e)
+        {
+
+
+            AddMouseEvent(
+                "MouseUp",
+                e.Button.ToString(),
+                e.X.ToString(),
+                e.Y.ToString(),
+                ""
+                );
+
+        }
+
+        void mouseHook_MouseDown(object sender, MouseEventArgs e)
+        {
+
+
+            AddMouseEvent(
+                "MouseDown",
+                e.Button.ToString(),
+                e.X.ToString(),
+                e.Y.ToString(),
+                ""
+                );
+
+
+        }
+
+        void mouseHook_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            SetXYLabel(e.X, e.Y);
+
+        }
+
+        void SetXYLabel(int x, int y)
+        {
+
+            curXYLabel.Text = String.Format("Current Mouse Point: X={0}, y={1}", x, y);
+
+        }
+
+        void AddMouseEvent(string eventType, string button, string x, string y, string delta)
+        {
+            string data = DateTime.Now.ToString();
+
+            string[] add = new string[]  {
+                        eventType,
+                        button,
+                        x,
+                        y,
+                        delta,
+                        data
+                    };
+
+            listView1.Items.Insert(0,
+                new ListViewItem(
+                   add
+                    ));
+
+        }
+
+        void AddKeyboardEvent(string eventType, string keyCode, string keyChar, string shift, string alt, string control)
+        {
+
+            listView2.Items.Insert(0,
+                 new ListViewItem(
+                     new string[]{
+                        eventType, 
+                        keyCode,
+                        keyChar,
+                        shift,
+                        alt,
+                        control
+                }));
+
+        }
+
+        private void TestForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+            // Not necessary anymore, will stop when application exits
+
+            //mouseHook.Stop();
+            //keyboardHook.Stop();
+
+        }
+
+
+    }
+}
